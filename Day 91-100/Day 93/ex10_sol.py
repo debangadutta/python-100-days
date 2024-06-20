0b088cf8cbc4c7eaf974a76368859fb28a8273a1
Debanga <93373450+debangadutta@users.noreply.github.com>
2024-06-20 22:06:47 +0200

    Update ex10_sol.py

--- Day 91-100/Day 93/ex10_sol.py
+++ Day 91-100/Day 93/ex10_sol.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 query = input("What type of news are you interested in? ")
-url = f"https://newsapi.org/v2/everything?q={query}&from=2023-01-28&sortBy=publishedAt&apiKey=dbe57b028aeb41e285a226a94865f7a7"
+url = f"https://newsapi.org/v2/everything?q={query}&from=2023-01-28&sortBy=publishedAt&apiKey=API_KEY"
 r = requests.get(url)
 news = json.loads(r.text)
 # print(news, type(news))
